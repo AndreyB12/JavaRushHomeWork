7bce4176f9ef0ddde90d4421c67e80a7c31a8030
Andrey Butko <andrey.butko@gmail.com>
2016-08-05 00:05:40 +0500

    New task

--- src/com/javarush/test/level20/lesson02/task03/Solution.java
+++ src/com/javarush/test/level20/lesson02/task03/Solution.java
@@ -14,11 +14,19 @@ public class Solution
 {
     public static Map<String, String> properties = new HashMap<>();
 
+    public static void main(String... args) throws IOException
+    {
+        Solution solution = new Solution();
+        solution.fillInPropertiesMap();
+
+    }
+
+
     public void fillInPropertiesMap() throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //FileReader fr = new FileReader(reader.readLine());
-        FileReader fr = new FileReader("e:\\test.properties");
+        FileReader fr = new FileReader(reader.readLine());
+        //FileReader fr = new FileReader("e:\\test.properties");
         reader.close();
         Properties props = new Properties();
         props.load(fr);
@@ -31,12 +39,23 @@ public class Solution
     public void save(OutputStream outputStream) throws Exception
     {
         PrintWriter pw = new PrintWriter(outputStream);
-        pw.print(this + ":");
-
+        pw.print(this + "|");
+        for (String key : properties.keySet())
+        {
+            pw.print(key + "|" + properties.get(key) + "|");
+        }
+        pw.println();
+        pw.flush();
     }
 
     public void load(InputStream inputStream) throws Exception
     {
-        //implement this method - реализуйте этот метод
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        String[] line;
+        line = br.readLine().split("\\|");
+        for (int i = 1; i < line.length; i += 2)
+        {
+            properties.put(line[i], line[i + 1]);
+        }
     }
 }
