2a92c78003dd4d9bf4f3961b0d57619734f4e888
Andrey Butko <andrey.butko@gmail.com>
2016-09-06 10:45:31 +0300

    Home work

--- src/com/javarush/test/level21/lesson02/task03/Solution.java
+++ src/com/javarush/test/level21/lesson02/task03/Solution.java
@@ -3,9 +3,15 @@ package com.javarush.test.level21.lesson02.task03;
 /* Все гениальное - просто!
 Упростить. Переменные не переименовывать, комментарии не оставлять.
 */
-public class Solution {
-    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+public class Solution
+{
+    public static void main(String...args){
+        System.out.println(calculate(true,false,true,true));
+    }
+
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d)
+    {
+        return c;
     }
 
 }
