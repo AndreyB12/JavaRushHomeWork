0e56b3ace04c87f0326ce9843049e257695c4947
Andrey Butko <andrey.butko@gmail.com>
2016-09-07 11:42:30 +0300

    Home work

--- src/com/javarush/test/level21/lesson08/task01/Solution.java
+++ src/com/javarush/test/level21/lesson08/task01/Solution.java
@@ -8,14 +8,17 @@ import java.util.Map;
 Данные в карте users также должны клонироваться.
 Метод main изменять нельзя.
 */
-public class Solution {
+public class Solution implements Cloneable
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Solution solution = new Solution();
         solution.users.put("Hubert", new User(172, "Hubert"));
         solution.users.put("Zapp", new User(41, "Zapp"));
         Solution clone = null;
-        try {
+        try
+        {
             clone = solution.clone();
             System.out.println(solution);
             System.out.println(clone);
@@ -23,18 +26,39 @@ public class Solution {
             System.out.println(solution.users);
             System.out.println(clone.users);
 
-        } catch (CloneNotSupportedException e) {
+        }
+        catch (CloneNotSupportedException e)
+        {
             e.printStackTrace(System.err);
         }
     }
 
     protected Map<String, User> users = new LinkedHashMap();
 
-    public static class User {
+    @Override
+    protected Solution clone() throws CloneNotSupportedException
+    {
+        Solution clone = new Solution();
+        for (String key : this.users.keySet())
+        {
+            clone.users.put(key,this.users.get(key).clone());
+        }
+        return  clone;
+    }
+
+    public static class User implements Cloneable
+    {
         int age;
         String name;
 
-        public User(int age, String name) {
+        @Override
+        protected User clone() throws CloneNotSupportedException
+        {
+            return new User(this.age, this.name);
+        }
+
+        public User(int age, String name)
+        {
             this.age = age;
             this.name = name;
         }
