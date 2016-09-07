6ab8bf7f905c9bd9e5cc8523ab2e20fd99687f0c
Andrey Butko <andrey.butko@gmail.com>
2016-09-07 22:40:48 +0300

    Home work

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -8,18 +8,30 @@ import java.util.ArrayList;
 public class Hippodrome
 {
     private ArrayList<Horse> horses = new ArrayList<>();
+    public static Hippodrome game;
 
     public ArrayList<Horse> getHorses()
     {
         return horses;
     }
 
-    public static void main(String...args)
+    public static void main(String... args)
     {
-        Hippodrome game = new Hippodrome();
-        game.getHorses().add(new Horse("Blacky",3,0));
-        game.getHorses().add(new Horse("Rudy",3,0));
-        game.getHorses().add(new Horse("Flower",3,0));
-
+        game = new Hippodrome();
+        game.getHorses().add(new Horse("Blacky", 3, 0));
+        game.getHorses().add(new Horse("Rudy", 3, 0));
+        game.getHorses().add(new Horse("Flower", 3, 0));
     }
+
+    public void run() throws InterruptedException
+    {
+        for (int i = 0; i <100 ; i++)
+        {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+    public void move(){}
+    public void print(){}
 }
