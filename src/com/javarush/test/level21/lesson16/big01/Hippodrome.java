86b9519df207f8b8a5f3109b4ebc1483d648d635
Andrey Butko <andrey.butko@gmail.com>
2016-09-07 22:31:02 +0300

    Home work

--- src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
+++ src/com/javarush/test/level21/lesson16/big01/Hippodrome.java
@@ -1,10 +1,19 @@
 package com.javarush.test.level21.lesson16.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by butkoav on 07.09.2016.
  */
 public class Hippodrome
 {
+    private ArrayList<Horse> horses = null;
+
+    public ArrayList<Horse> getHorses()
+    {
+        return horses;
+    }
+
     public static void main(String...args)
     {
 
