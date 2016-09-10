7a024193348917777026c0942cd2787babb40be0
Andrey Butko <andrey.butko@gmail.com>
2016-09-11 00:24:59 +0300

    Home work

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.test.level22.lesson09.task03;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /* Составить цепочку слов
 В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
@@ -34,23 +37,52 @@ public class Solution
                 if (fr.ready()) sb.append(" ");
             }
             words = sb.toString().split(" ");
-        }
+          }
         catch (Exception e)
         {
             throw e;
         }
         if (words == null) return;
 
-        StringBuilder result = getLine();
+        StringBuilder result = getLine(words);
         System.out.println(result.toString());
     }
 
     public static StringBuilder getLine(String... words)
     {
+        Arrays.sort(words);
+        List<String> line = new LinkedList<>();
         StringBuilder result = new StringBuilder();
+        for (int i = 0; i < words.length; i++)
+        {
+            line.add(words[i]);
+            if (findNext(words[i], words, line))
+            {
+                break;
+            } else line.remove(line.size() - 1);
+        }
 
-
+        for (String word : line)
+        {
+            if (result.length() > 0) result.append(" ");
+            result.append(word);
+        }
 
         return result;
+    }
+
+    private static boolean findNext(String word, String[] words, List<String> line)
+    {
+        if (line.size() == words.length) return true;
+        for (String w : words)
+        {
+            if (w.toLowerCase().charAt(0) == word.toLowerCase().charAt(word.length() - 1) && !line.contains(w))
+            {
+                line.add(w);
+                if (findNext(w, words, line)) return true;
+                else line.remove(line.size() - 1);
+            }
+        }
+        return false;
     }
 }
