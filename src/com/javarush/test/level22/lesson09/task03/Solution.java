b7fddb48249dd29ebfff0a431edd905aae2a358a
Andrey Butko <andrey.butko@gmail.com>
2016-09-10 23:29:21 +0300

    Home work

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level22.lesson09.task03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
 /* Составить цепочку слов
 В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
 В методе getLine используя StringBuilder расставить все слова в таком порядке,
@@ -15,14 +19,34 @@ package com.javarush.test.level22.lesson09.task03;
 Результат:
 Амстердам Мельбурн Нью-Йорк Киев Вена
 */
-public class Solution {
-    public static void main(String[] args) {
-        //...
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        String[] words = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fr = new BufferedReader(new FileReader(reader.readLine())))
+        {
+            StringBuilder sb = new StringBuilder();
+            while (fr.ready())
+            {
+                sb.append(fr.readLine());
+                if (fr.ready()) sb.append(" ");
+            }
+            words = sb.toString().split(" ");
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }
+        if (words == null) return;
+
         StringBuilder result = getLine();
         System.out.println(result.toString());
     }
 
-    public static StringBuilder getLine(String... words) {
-          return null;
+    public static StringBuilder getLine(String... words)
+    {
+        return null;
     }
 }
