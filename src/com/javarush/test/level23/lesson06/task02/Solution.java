cf4d414c9082d630e0c998619469c460c85e6c6c
Andrey Butko <andrey.butko@gmail.com>
2016-09-17 14:47:15 +0300

    Home work

--- src/com/javarush/test/level23/lesson06/task02/Solution.java
+++ src/com/javarush/test/level23/lesson06/task02/Solution.java
@@ -4,45 +4,67 @@ package com.javarush.test.level23.lesson06.task02;
 Отрефакторите класс Solution: вынесите все константы в public вложенный(nested) класс Constants.
 Запретите наследоваться от Constants.
 */
-public class Solution {
+public class Solution
+{
 
-    public class ServerNotAccessibleException extends Exception {
-        public ServerNotAccessibleException() {
-            super("Server is not accessible for now.");
+    public class ServerNotAccessibleException extends Exception
+    {
+        public ServerNotAccessibleException()
+        {
+            super(Constants.SERVER_NOT_ACCESSIBLE);
         }
 
-        public ServerNotAccessibleException(Throwable cause) {
-            super("Server is not accessible for now.", cause);
-        }
-    }
-
-    public class UnauthorizedUserException extends Exception {
-        public UnauthorizedUserException() {
-            super("User is not authorized.");
-        }
-
-        public UnauthorizedUserException(Throwable cause) {
-            super("User is not authorized.", cause);
+        public ServerNotAccessibleException(Throwable cause)
+        {
+            super(Constants.SERVER_NOT_ACCESSIBLE, cause);
         }
     }
 
-    public class BannedUserException extends Exception {
-        public BannedUserException() {
-            super("User is banned.");
+    public class UnauthorizedUserException extends Exception
+    {
+        public UnauthorizedUserException()
+        {
+            super(Constants.USER_NOT_AUTHORIZED);
         }
 
-        public BannedUserException(Throwable cause) {
-            super("User is banned.", cause);
+        public UnauthorizedUserException(Throwable cause)
+        {
+            super(Constants.USER_NOT_AUTHORIZED, cause);
         }
     }
 
-    public class RestrictionException extends Exception {
-        public RestrictionException() {
-            super("Access is denied.");
+    public class BannedUserException extends Exception
+    {
+        public BannedUserException()
+        {
+            super(Constants.USER_BANNED);
         }
 
-        public RestrictionException(Throwable cause) {
-            super("Access is denied.", cause);
+        public BannedUserException(Throwable cause)
+        {
+            super(Constants.USER_BANNED, cause);
         }
+    }
+
+    public class RestrictionException extends Exception
+    {
+        public RestrictionException()
+        {
+            super(Constants.ACCESS_DENIED);
+        }
+
+        public RestrictionException(Throwable cause)
+        {
+            super(Constants.ACCESS_DENIED, cause);
+        }
+    }
+
+    public static final class Constants
+    {
+        final public static String SERVER_NOT_ACCESSIBLE = "Server is not accessible for now.";
+        final public static String USER_NOT_AUTHORIZED = "User is not authorized.";
+        final public static String USER_BANNED = "User is banned.";
+        final public static String ACCESS_DENIED = "Access is denied.";
+
     }
 }
