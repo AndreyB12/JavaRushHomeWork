edef3a283b7a7844e5a28a432ce540a9e7b99aa5
Andrey Butko <andrey.butko@gmail.com>
2016-09-18 18:05:06 +0300

    Home work

--- src/com/javarush/test/level23/lesson13/big01/SnakeDirection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeDirection.java
@@ -3,6 +3,7 @@ package com.javarush.test.level23.lesson13.big01;
 /**
  * Created by butkoav on 18.09.2016.
  */
-public class SnakeDirection
+public enum SnakeDirection
 {
+    UP, RIGHT, DOWN, LEFT
 }
