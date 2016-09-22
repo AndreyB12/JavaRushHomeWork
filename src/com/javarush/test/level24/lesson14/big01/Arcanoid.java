4192225c2d15dd1965689a5667efc89bd8def2df
Andrey Butko <andrey.butko@gmail.com>
2016-09-22 21:57:35 +0300

    Home work

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -5,4 +5,34 @@ package com.javarush.test.level24.lesson14.big01;
  */
 public class Arcanoid
 {
+    int height,width;
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public Arcanoid(int height, int width)
+    {
+        this.height = height;
+        this.width = width;
+    }
+
+    public static void main(String...args)
+    {}
 }
