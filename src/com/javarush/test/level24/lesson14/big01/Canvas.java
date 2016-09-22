c3ef0c98272bd0e3919fb06b391ffbc53496be0e
Andrey Butko <andrey.butko@gmail.com>
2016-09-22 22:44:59 +0300

    Home work

--- src/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -48,4 +48,25 @@ public class Canvas
             }
         }
     }
+
+    public void clear()
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print()
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            System.out.println(matrix[i]);
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
