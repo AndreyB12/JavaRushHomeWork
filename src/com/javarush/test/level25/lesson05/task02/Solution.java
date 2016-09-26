f48a7b24639a7b1e75c0b1ada93ccda69ece0adc
Andrey Butko <andrey.butko@gmail.com>
2016-09-26 23:09:20 +0300

    Update tasks

--- src/com/javarush/test/level25/lesson05/task02/Solution.java
+++ src/com/javarush/test/level25/lesson05/task02/Solution.java
@@ -8,27 +8,51 @@ super secret key, Thread-0, it's an example
 2. Разберитесь в последовательности выполняемого кода и обеспечьте логирование возникновения исключения в п.1.
 3. Метод main в тестировании не участвует.
 */
-public class Solution {
+public class Solution
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException
+    {
         MyThread myThread = new Solution().new MyThread("super secret key");
         myThread.start();
+
     }
 
-    public class MyThread extends Thread {
+    public class MyThread extends Thread
+    {
         private String secretKey;
 
-        public MyThread(String secretKey) {
+        public MyThread(String secretKey)
+        {
             this.secretKey = secretKey;
             setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
-            setDaemon(true);
         }
 
         @Override
-        public void run() {
+        public void run()
+        {
             throw new NullPointerException("it's an example");
         }
-    }
 
+
+        private class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
+        {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e)
+            {
+                try
+                {
+                    t.sleep(500);
+                    System.out.println(String.format("%s, %s, %s", ((MyThread) t).secretKey, t.getName(), e.getMessage()));
+                }
+                catch (Exception e1)
+                {
+                    System.out.println(e1);
+                }
+            }
+        }
+
+    }
 }
 
