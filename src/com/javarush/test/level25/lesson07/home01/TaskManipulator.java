32f6a4f4852d146f3e8db2417cf1b4d1d0573aed
Andrey Butko <andrey.butko@gmail.com>
2016-09-27 22:44:16 +0300

    Update tasks

--- src/com/javarush/test/level25/lesson07/home01/TaskManipulator.java
+++ src/com/javarush/test/level25/lesson07/home01/TaskManipulator.java
@@ -1,7 +1,41 @@
 package com.javarush.test.level25.lesson07.home01;
 
-public class TaskManipulator implements Runnable {
+public class TaskManipulator implements Runnable, CustomThreadManipulator
+{
+    Thread t;
+    String name;
     @Override
-    public void run() {
+    public void run()
+    {
+        try
+        {
+            while (!t.isInterrupted())
+            {
+
+                System.out.println(name);
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException e)
+        {
+
+        }
+
+
+    }
+
+    @Override
+    public void start(String threadName)
+    {
+        name = threadName;
+        t = new Thread(this);
+        t.setName(threadName);
+        t.start();
+    }
+
+    @Override
+    public void stop()
+    {
+        t.interrupt();
     }
 }
