f5b3206274f4f92c22c2f97c0446389deac2e6f3
Andrey Butko <andrey.butko@gmail.com>
2016-10-06 22:53:40 +0300

    Update tasks

--- src/com/javarush/test/level25/lesson16/big01/Rocket.java
+++ src/com/javarush/test/level25/lesson16/big01/Rocket.java
@@ -1,21 +1,28 @@
 package com.javarush.test.level25.lesson16.big01;
 
 /**
- * Created by butkoav on 06.10.2016.
+ * Класс для объектов-ракета
  */
-public class Rocket extends BaseObject
+public class Rocket  extends BaseObject
 {
+
     public Rocket(double x, double y)
     {
         super(x, y, 1);
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas)
     {
-        canvas.setPoint(x, y, 'R');
+        canvas.setPoint(x,y,'R');
     }
 
+    /**
+     * Двигаем себя вверх на один ход.
+     */
     @Override
     public void move()
     {
