1644d9e3a79e8d8ad4a78ad3abb0bff7c8228471
Andrey Butko <andrey.butko@gmail.com>
2016-10-11 23:53:25 +0300

    Update tasks

---
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -0,0 +1,11 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by butkoav on 11.10.2016.
+ */
+public class CashMachine
+{
+    public static void main(String... args)
+    {
+    }
+}
---
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by butkoav on 11.10.2016.
+ */
+public class ConsoleHelper
+{
+}
