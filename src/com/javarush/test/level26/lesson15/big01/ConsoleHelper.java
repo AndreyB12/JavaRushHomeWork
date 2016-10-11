1cde665773388bfd5c4d2b2e689e2ce5741c128e
Andrey Butko <andrey.butko@gmail.com>
2016-10-12 00:04:39 +0300

    Update tasks

--- src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -1,8 +1,31 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by butkoav on 11.10.2016.
  */
 public class ConsoleHelper
 {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message)
+    {
+        System.out.println(message);
+    }
+
+    public static String readString()
+    {
+        String result = "";
+        try
+        {
+            result = reader.readLine();
+        }
+        catch (IOException e)
+        {
+        }
+        return result;
+    }
 }
