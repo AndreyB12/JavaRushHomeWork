6a95898c55ed4a63bb1704f51026c8e88a941db3
Andrey Butko <andrey.butko@gmail.com>
2016-10-29 00:06:27 +0500

    Update tasks

---
+++ src/com/javarush/test/level26/lesson15/big01/command/LoginCommand.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level26.lesson15.big01.command;
+
+import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+
+/**
+ * Created by butkoav on 29.10.2016.
+ */
+public class LoginCommand implements Command
+{
+    @Override
+    public void execute() throws InterruptOperationException
+    {
+
+    }
+}
