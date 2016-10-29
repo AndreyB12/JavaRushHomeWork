7c69b2d7a5b93209cd3251ca08d917865f01f12e
Andrey Butko <andrey.butko@gmail.com>
2016-10-29 17:51:16 +0500

    Update tasks

--- src/com/javarush/test/level26/lesson15/big01/command/ExitCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/ExitCommand.java
@@ -3,15 +3,22 @@ package com.javarush.test.level26.lesson15.big01.command;
 import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Created by butkoav on 19.10.2016.
  */
 class ExitCommand implements Command
 {
+    private ResourceBundle res = ResourceBundle.getBundle("com.javarush.test.level26.lesson15.big01.resources.exit", Locale.ENGLISH);
+
     @Override
     public void execute() throws InterruptOperationException
     {
-      //  if (ConsoleHelper.askAreYouSure()) throw new InterruptOperationException();
+        ConsoleHelper.writeMessage(res.getString("before"));
+        ConsoleHelper.writeMessage(res.getString("exit.question.y.n"));
+        if (res.getString("y").toLowerCase().equals(ConsoleHelper.readString().toLowerCase())) throw new InterruptOperationException();
     }
 
     ExitCommand()
