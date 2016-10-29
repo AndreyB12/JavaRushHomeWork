2fc3ef31b29d9d4211fd1195cc1d68ffa91abc64
Andrey Butko <andrey.butko@gmail.com>
2016-10-29 17:47:29 +0500

    Update tasks

--- src/com/javarush/test/level26/lesson15/big01/command/InfoCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/InfoCommand.java
@@ -4,15 +4,20 @@ import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level26.lesson15.big01.CurrencyManipulator;
 import com.javarush.test.level26.lesson15.big01.CurrencyManipulatorFactory;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 /**
  * Created by butkoav on 19.10.2016.
  */
 class InfoCommand implements Command
 {
+    private ResourceBundle res = ResourceBundle.getBundle("com.javarush.test.level26.lesson15.big01.resources.info", Locale.ENGLISH);
     @Override
     public void execute()
     {
+        ConsoleHelper.writeMessage(res.getString("before"));
         boolean hasMoney = false;
         for (CurrencyManipulator currency : CurrencyManipulatorFactory.getAllCurrencyManipulators())
         {
@@ -22,7 +27,7 @@ class InfoCommand implements Command
                 ConsoleHelper.writeMessage(String.format("%s - %d", currency.getCurrencyCode(), currency.getTotalAmount()));
             }
         }
-        if (!hasMoney) ConsoleHelper.writeMessage("No money available.");
+        if (!hasMoney) ConsoleHelper.writeMessage(res.getString("no.money"));
     }
 
     InfoCommand()
