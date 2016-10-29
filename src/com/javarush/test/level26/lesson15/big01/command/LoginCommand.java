de65e8b4f7e307404cc8c6ba77e60ea332c88aed
Andrey Butko <andrey.butko@gmail.com>
2016-10-29 17:42:39 +0500

    Update tasks

--- src/com/javarush/test/level26/lesson15/big01/command/LoginCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/LoginCommand.java
@@ -3,13 +3,14 @@ package com.javarush.test.level26.lesson15.big01.command;
 import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
 import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 /**
  * Created by butkoav on 29.10.2016.
  */
 public class LoginCommand implements Command
 {
-    private static final long cardID = 123456789012L;
-    private static final int pin = 1234;
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle("com.javarush.test.level26.lesson15.big01.resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException
@@ -18,7 +19,7 @@ public class LoginCommand implements Command
         {
             String cID;
             String cPIN;
-            ConsoleHelper.writeMessage("Enter card number (12 nums) and a pin(4 nums):");
+            ConsoleHelper.writeMessage("Enter card number (12 nums):");
             cID = ConsoleHelper.readString();
             if (!cID.matches("^[0-9]{12}$"))
             {
@@ -32,9 +33,14 @@ public class LoginCommand implements Command
                 ConsoleHelper.writeMessage("Data not valid!");
                 continue;
             }
-            if (!(Long.valueOf(cID) == cardID && Integer.valueOf(cPIN) == pin))
+            if (!validCreditCards.containsKey(cID))
             {
-                ConsoleHelper.writeMessage("Wrong CardID or PIN");
+                ConsoleHelper.writeMessage("Wrong CardID");
+                continue;
+            }
+            if (!validCreditCards.getString(cID).equals(cPIN))
+            {
+                ConsoleHelper.writeMessage("Wrong Pin");
                 continue;
             }
             ConsoleHelper.writeMessage("Welcome!");
