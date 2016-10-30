0730345907ba673f26c9423020c293dbe4262fdd
Andrey Butko <andrey.butko@gmail.com>
2016-10-30 22:28:38 +0500

    Update tasks

--- src/com/javarush/test/level27/lesson09/home02/Person.java
+++ src/com/javarush/test/level27/lesson09/home02/Person.java
@@ -1,21 +1,29 @@
 package com.javarush.test.level27.lesson09.home02;
 
-public class Person implements Runnable {
+public class Person implements Runnable
+{
     private final Mail mail;
 
-    public Person(Mail mail) {
+    public Person(Mail mail)
+    {
         this.mail = mail;
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
         String name = Thread.currentThread().getName();
-        try {
+        try
+        {
             Thread.sleep(1000);
-            //сделайте что-то тут - do something here
-            mail.setText("Person [" + name + "] has written an email 'AAA'");
-            //сделайте что-то тут - do something here
-        } catch (InterruptedException e) {
+            synchronized (mail)
+            {
+                mail.setText("Person [" + name + "] has written an email 'AAA'");
+                mail.notify();
+            }
+        }
+        catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
     }
