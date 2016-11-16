81abaee0f7bbe675445ac291a1601773c9691915
Andrey Butko <andrey.butko@gmail.com>
2016-11-16 22:58:55 +0200

    Update

--- src/com/javarush/test/level30/lesson02/task01/Solution.java
+++ src/com/javarush/test/level30/lesson02/task01/Solution.java
@@ -4,16 +4,25 @@ package com.javarush.test.level30.lesson02.task01;
 Используя метод Integer.parseInt(String, int) реализуйте логику метода convertToDecimalSystem,
 который должен переводить переданную строку в десятичное число и возвращать его в виде строки.
 */
-public class Solution {
+public class Solution
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println(convertToDecimalSystem("0x16")); //22
         System.out.println(convertToDecimalSystem("012"));  //10
         System.out.println(convertToDecimalSystem("0b10")); //2
         System.out.println(convertToDecimalSystem("62"));   //62
     }
 
-    public static String convertToDecimalSystem(String s) {
-        return s;
+    public static String convertToDecimalSystem(String s)
+    {
+        if (s.substring(0, 2).equalsIgnoreCase("0x"))
+            return String.valueOf(Integer.parseInt(s.substring(2, s.length()), 16));
+        if (s.substring(0, 2).equalsIgnoreCase("0b"))
+            return String.valueOf(Integer.parseInt(s.substring(2, s.length()), 2));
+        if (s.charAt(0) == '0')
+            return String.valueOf(Integer.parseInt(s.substring(1, s.length()), 8));
+        return String.valueOf(Integer.parseInt(s));
     }
 }
