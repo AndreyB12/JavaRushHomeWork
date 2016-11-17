3df7ad862347387ca42283bd7a2ffcaa2a9032df
Andrey Butko <andrey.butko@gmail.com>
2016-11-17 16:17:12 +0200

    Update

--- src/com/javarush/test/level30/lesson06/task01/Solution.java
+++ src/com/javarush/test/level30/lesson06/task01/Solution.java
@@ -8,39 +8,54 @@ import java.util.List;
 Сохраните логику вывода данных.
 Метод main не участвует в тестировании.
 */
-public class Solution {
-    public static void main(String[] args) {
+public class Solution
+{
+    public static void main(String[] args)
+    {
         checkAFlag(new D());
     }
 
-    public static void checkAFlag(D d) {
-        if(d.cs.get(0).bs.get(0).as.get(0).flag){
+    public static void checkAFlag(D d)
+    {
+        if (d != null && d.cs != null && !d.cs.isEmpty()
+                && d.cs.get(0) != null && d.cs.get(0).bs != null && !d.cs.get(0).bs.isEmpty()
+                && d.cs.get(0).bs.get(0) != null && d.cs.get(0).bs.get(0).as != null && !d.cs.get(0).bs.get(0).as.isEmpty()
+                && d.cs.get(0).bs.get(0).as.get(0) != null && d.cs.get(0).bs.get(0).as.get(0).flag)
+        {
             System.out.println("A's flag is true");
-        } else { //all other cases
+        } else
+        { //all other cases
             System.out.println("Oops!");
         }
     }
 
-    static class A {
+    static class A
+    {
         boolean flag = true;
     }
 
-    static class B {
+    static class B
+    {
         List<A> as = new ArrayList<>();
+
         {
             as.add(new A());
         }
     }
 
-    static class C {
+    static class C
+    {
         List<B> bs = new ArrayList<>();
+
         {
             bs.add(new B());
         }
     }
 
-    static class D {
+    static class D
+    {
         List<C> cs = new ArrayList<>();
+
         {
             cs.add(new C());
         }
