ba90bd548bf20ce69311a33620bd51810ee7ae66
Andrey Butko <andrey.butko@gmail.com>
2016-11-20 14:02:36 +0200

    Update

---
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level28.lesson15.big01.model;
+
+/**
+ * Created by butkoav on 19.11.2016.
+ */
+public class HHStrategy implements Strategy
+{
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+}
