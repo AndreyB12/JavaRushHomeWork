f9e9e29a068998765630d63dccd8505370d20de7
Andrey Butko <andrey.butko@gmail.com>
2016-11-30 22:12:55 +0200

    Update

---
+++ src/com/javarush/test/level30/lesson15/big01/Message.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level30.lesson15.big01;
+
+/**
+ * Created by butkoav on 30.11.2016.
+ */
+public class Message
+{
+}
--- src/com/javarush/test/level31/lesson02/home01/Solution.java
+++ src/com/javarush/test/level31/lesson02/home01/Solution.java
@@ -1,5 +1,14 @@
 package com.javarush.test.level31.lesson02.home01;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /* Проход по дереву файлов
 1. На вход метода main подаются два параметра.
 Первый - path - путь к директории, второй - resultFileAbsolutePath - имя файла, который будет содержать результат.
@@ -12,8 +21,59 @@ package com.javarush.test.level31.lesson02.home01;
 2.3. Удалить директории без файлов (пустые).
 Все файлы имеют расширение txt.
 */
-public class Solution {
-    public static void main(String[] args) {
+public class Solution
+{
 
+    static List<File> files = new ArrayList<>();
+    static File resultFile;
+    public static void main(String[] args) throws IOException
+    {
+
+        File root = new File(args[0]);
+        resultFile = new File(args[1]);
+
+        processFiles(root);
+        Collections.sort(files, new Comparator<File>()
+        {
+            @Override
+            public int compare(File o1, File o2)
+            {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        File newOutputFile = new File(resultFile.getParent() + "/allFilesContent.txt");
+        resultFile.renameTo(newOutputFile);
+
+        try (FileOutputStream os = new FileOutputStream(newOutputFile))
+        {
+            for (File file : files)
+            {
+                byte[] b = new byte[50];
+                int i;
+                FileInputStream is = new FileInputStream(file);
+                i = is.read(b);
+                os.write(b, 0, i);
+                os.write(10);
+            }
+        }
+        catch (Exception e)
+        {
+        }
+    }
+
+    public static void processFiles(File root)
+    {
+        for (File file : root.listFiles())
+        {
+            if (file.isDirectory())
+            {
+                processFiles(file);
+                if (file.listFiles().length == 0) file.delete();
+            } else if (!file.getAbsolutePath().equalsIgnoreCase(resultFile.getAbsolutePath()))
+            {
+                if (file.length() > 50) file.delete();
+                else files.add(file);
+            }
+        }
     }
 }
