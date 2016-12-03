39bfc2ef8e6a8bf16fdb8047de60aef973a7577e
Andrey Butko <andrey.butko@gmail.com>
2016-12-03 22:21:00 +0200

    Update

--- src/Test/TestModule.java
+++ src/Test/TestModule.java
@@ -1,76 +1,17 @@
 package Test;
 
-//import static com.javarush.test.level08.lesson08.task03.Solution.createMap;
+
+import java.io.IOException;
+
+import static com.javarush.test.level31.lesson04.home01.Solution.copy;
 
 /**
  * Created by butkoav on 21.04.2016.
  */
 public class TestModule
 {
-    private static String s;
-    private static TestClass testClass = new TestClass(0);
-
-    public static void main(String[] aa)
+    public static void main(String[] aa) throws IOException
     {
-       Thread t = new T1();
-        t.start();
-        try
-        {
-            Thread.sleep(500);
-        }
-        catch (InterruptedException e)
-        {}
-        for (int i = 0; i < 10; i++)
-        {
-            synchronized (testClass)
-            {
-                testClass.setI(testClass.getI() + 1);
-            }
-            System.out.println(testClass.getI());
-        }
+        copy("e:\\temp\\source.txt","e:\\temp\\destination.txt");
     }
-    private static class T1 extends Thread
-    {
-        @Override
-        public void run()
-        {
-            test(testClass);
-        }
-        private synchronized void test(TestClass tc)
-        {
-            //synchronized (tc)
-            //{
-                System.out.println(testClass.getI());
-                try
-                {
-                    sleep(1000);
-                }
-                catch (InterruptedException e)
-                {
-                }
-                System.out.println(testClass.getI());
-           // }
-        }
-    }
-
-    public static class TestClass
-    {
-        private int i;
-
-        public TestClass(int i)
-        {
-            this.i = i;
-        }
-
-        public int getI()
-        {
-            return i;
-        }
-
-        public void setI(int i)
-        {
-            this.i = i;
-        }
-    }
-
 }
--- src/com/javarush/test/level31/lesson04/home01/Solution.java
+++ src/com/javarush/test/level31/lesson04/home01/Solution.java
@@ -16,18 +16,25 @@ import java.util.List;
 ГЛАВНОЕ УСЛОВИЕ:
 Никаких других импортов!
 */
-public class Solution {
-    public static byte[] readBytes(String fileName) throws IOException {
-        return null;
+public class Solution
+{
+    public static byte[] readBytes(String fileName) throws IOException
+    {
+        return Files.readAllBytes(Paths.get(fileName));
     }
 
-    public static List<String> readLines(String fileName) throws IOException {
-        return null;
+    public static List<String> readLines(String fileName) throws IOException
+    {
+        return Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
     }
 
-    public static void writeBytes(String fileName, byte[] bytes) throws IOException {
+    public static void writeBytes(String fileName, byte[] bytes) throws IOException
+    {
+        Files.write(Paths.get(fileName), bytes);
     }
 
-    public static void copy(String resourceFileName, String destinationFileName) throws IOException {
+    public static void copy(String resourceFileName, String destinationFileName) throws IOException
+    {
+        Files.copy(Paths.get(resourceFileName), Paths.get(destinationFileName));
     }
 }
