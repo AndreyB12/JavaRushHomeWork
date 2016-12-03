66dff485fcca5961d5431b9a29f13f10ca298efe
Andrey Butko <andrey.butko@gmail.com>
2016-12-04 00:46:44 +0200

    Update

--- src/com/javarush/test/level31/lesson04/home02/Solution.java
+++ src/com/javarush/test/level31/lesson04/home02/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.test.level31.lesson04.home02;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -16,33 +15,58 @@ import java.util.List;
 D:/mydir/BCD.zip
 Метод main не участвует в тестировании
 */
-public class Solution extends SimpleFileVisitor<Path> {
-    public static void main(String[] args) throws IOException {
+public class Solution extends SimpleFileVisitor<Path>
+{
+    public static void main(String[] args) throws IOException
+    {
         EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
         final Solution solution = new Solution();
         Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
 
         List<String> result = solution.getArchived();
         System.out.println("All archived files:");
-        for (String path : result) {
+        for (String path : result)
+        {
             System.out.println("\t" + path);
         }
 
         List<String> failed = solution.getFailed();
         System.out.println("All failed files:");
-        for (String path : failed) {
+        for (String path : failed)
+        {
             System.out.println("\t" + path);
         }
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+    {
+        if (!attrs.isDirectory() &&
+                (file.toString().toLowerCase().endsWith(".zip")
+                        || file.toString().toLowerCase().endsWith(".rar")))
+        {
+            archived.add(file.toString());
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException
+    {
+        failed.add(file.toString());
+        return FileVisitResult.SKIP_SUBTREE;
     }
 
     private List<String> archived = new ArrayList<>();
     private List<String> failed = new ArrayList<>();
 
-    public List<String> getArchived() {
+    public List<String> getArchived()
+    {
         return archived;
     }
 
-    public List<String> getFailed() {
+    public List<String> getFailed()
+    {
         return failed;
     }
 }
