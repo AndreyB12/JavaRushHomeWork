14e7f8ab2b85c9a4690458f3a5689164e71746a4
Andrey Butko <andrey.butko@gmail.com>
2016-12-03 21:41:53 +0200

    Update

--- src/com/javarush/test/level31/lesson02/home02/Solution.java
+++ src/com/javarush/test/level31/lesson02/home02/Solution.java
@@ -1,7 +1,11 @@
 package com.javarush.test.level31.lesson02.home02;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /* Находим все файлы
 Реализовать логику метода getFileTree, который должен в директории root найти список всех файлов включая вложенные.
@@ -9,9 +13,26 @@ import java.util.List;
 Верните список всех путей к найденным файлам, путь к директориям возвращать не надо.
 Путь должен быть абсолютный.
 */
-public class Solution {
-    public static List<String> getFileTree(String root) throws IOException {
-        return null;
+public class Solution
+{
+    public static List<String> getFileTree(String root) throws IOException
+    {
+        List<String> files = new ArrayList<>();
+        Queue<File> queue = new PriorityQueue<>();
+
+        queue.add(new File(root));
+
+
+        while (!queue.isEmpty())
+        {
+            File file = queue.poll();
+            for (File subFile : file.listFiles())
+            {
+                if (subFile.isDirectory()) queue.add(subFile);
+                else files.add(subFile.getAbsolutePath());
+            }
+        }
+        return files;
 
     }
 }
