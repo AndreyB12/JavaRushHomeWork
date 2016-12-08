5def2905f1837f83b4e61e2ba30191eeb52ccd46
Andrey Butko <andrey.butko@gmail.com>
2016-12-08 15:29:56 +0200

    Update

--- src/com/javarush/test/level31/lesson06/home01/Solution.java
+++ src/com/javarush/test/level31/lesson06/home01/Solution.java
@@ -1,6 +1,12 @@
 package com.javarush.test.level31.lesson06.home01;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /* Добавление файла в архив
 В метод main приходит список аргументов.
@@ -26,7 +32,66 @@ b.txt
 а потом записать в архив все энтри вместе с добавленным файлом.
 Пользоваться файловой системой нельзя.
 */
-public class Solution {
-    public static void main(String[] args) throws IOException {
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        File newFile = new File(args[0]);
+
+        String zipFile = args[1];
+
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+        Map<ZipEntry, byte[]> entryList = new HashMap<>();
+        ZipEntry entry;
+        while ((entry = zis.getNextEntry()) != null)
+        {
+            if (!entry.isDirectory())
+            {
+                ByteArrayOutputStream byteOS = new ByteArrayOutputStream();
+                byte[] bytes = new byte[1024];
+                int i;
+                while ((i = zis.read(bytes)) > 0)
+                {
+                    byteOS.write(bytes, 0, i);
+                }
+                entryList.put(entry, byteOS.toByteArray());
+                byteOS.close();
+            }
+        }
+        zis.close();
+
+        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipFile));
+
+        boolean contains = false;
+        for (Map.Entry<ZipEntry, byte[]> entr : entryList.entrySet())
+        {
+            String name = entr.getKey().getName().split("/")[entr.getKey().getName().split("/").length - 1];
+            if (name.equalsIgnoreCase(newFile.getName()))
+            {
+                ZipEntry newZipEntry = new ZipEntry("new/" + newFile.getName());
+                newZipEntry.setSize(newFile.length());
+                zos.putNextEntry(newZipEntry);
+                Files.copy(newFile.toPath(), zos);
+                zos.closeEntry();
+                contains = true;
+            } else
+            {
+                zos.putNextEntry(entr.getKey());
+                if (entr.getValue() != null) zos.write(entr.getValue());
+                zos.closeEntry();
+            }
+        }
+
+   /*     if (!contains)
+        {
+
+            ZipEntry newZipEntry = new ZipEntry("new/" + newFile.getName());
+            newZipEntry.setSize(newFile.length());
+            zos.putNextEntry(newZipEntry);
+            Files.copy(newFile.toPath(), zos);
+            zos.closeEntry();
+        }*/
+
+        zos.close();
     }
 }
