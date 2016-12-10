722ea10be64aa71a13730a5d712a6d61f267ca91
Andrey Butko <andrey.butko@gmail.com>
2016-12-10 13:40:46 +0200

    Update

---
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level31.lesson15.big01;
+
+import java.nio.file.Path;
+
+/**
+ * Created by butkoav on 10.12.2016.
+ */
+public class ZipFileManager
+{
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile)
+    {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception
+    {
+
+    }
+
+}
