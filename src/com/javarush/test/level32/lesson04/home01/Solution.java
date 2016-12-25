80393be426eaed46fb53110ff40927851b44c122
Andrey Butko <andrey.butko@gmail.com>
2016-12-25 21:42:23 +0200

    Update

--- src/com/javarush/test/level32/lesson04/home01/Solution.java
+++ src/com/javarush/test/level32/lesson04/home01/Solution.java
@@ -1,8 +1,8 @@
 package com.javarush.test.level32.lesson04.home01;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 
 /* Читаем из потока
@@ -10,13 +10,29 @@ import java.io.StringWriter;
 Возвращаемый объект ни при каких условиях не должен быть null.
 Метод main не участвует в тестировании.
 */
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        //  StringWriter writer = getAllDataFromInputStream(new FileInputStream("E:\\test.txt"));
+        StringWriter writer = getAllDataFromInputStream(null);
         System.out.println(writer.toString());
     }
 
-    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
-        return null;
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException
+    {
+        StringWriter sw = new StringWriter();
+        if (is == null) return sw;
+        InputStreamReader isr = new InputStreamReader(is);
+
+        char[] bufer = new char[1024];
+        int cnt;
+        while ((cnt = isr.read(bufer)) > 0)
+        {
+            sw.write(bufer, 0, cnt);
+        }
+
+        return sw;
+
     }
 }
