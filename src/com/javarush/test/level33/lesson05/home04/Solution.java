74a2a43a786ce94c7e64097de2f5fb852c12fe93
Andrey Butko <andrey.butko@gmail.com>
2017-01-04 21:35:22 +0200

    Update

---
+++ src/com/javarush/test/level33/lesson05/home04/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.test.level33.lesson05.home04;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/* Конвертация из одного класса в другой используя JSON
+НЕОБХОДИМО: подключенные библиотеки Jackson Core, Bind и Annotation версии 2.6.1
+
+Два класса имеют одинаковые поля, но не имеют общий суперкласс. Пример, классы First и Second.
+Реализовать логику метода convertOneToAnother, который должен возвращать объект класса resultClassObject,
+значения полей которого равны значениям полей в объекте one.
+Известно, что у классов есть Json аннотация, у которой значение проперти равно имени класса в нижнем регистре.
+На примере класса First, это className="first"
+Классы First и Second не участвуют в тестировании, они предоставлены в качестве тестовых данных.
+*/
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        Second s = (Second) convertOneToAnother(new First(), Second.class);
+        First f = (First) convertOneToAnother(new Second(), First.class);
+    }
+
+    public static Object convertOneToAnother(Object one, Class resultClassObject) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        StringReader sr = new StringReader(mapper.writeValueAsString(one));
+        JsonNode node = mapper.readTree(sr);
+        String key  = node.fieldNames().next();
+        ObjectNode oNode = (ObjectNode) node;
+        oNode.put(key,resultClassObject.getSimpleName().toLowerCase());
+        return mapper.treeToValue(node,resultClassObject);
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = First.class, name = "first"))
+    public static class First
+    {
+        public int i;
+        public String name;
+    }
+
+    @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "className")
+    @JsonSubTypes(@JsonSubTypes.Type(value = Second.class, name = "second"))
+    public static class Second
+    {
+        public int i;
+        public String name;
+    }
+}
