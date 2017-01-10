49f818f1083fd8dacad7efd2772a136865e42e74
Andrey Butko <andrey.butko@gmail.com>
2017-01-10 23:06:52 +0200

    Update

--- src/com/javarush/test/level34/lesson06/task01/Solution.java
+++ src/com/javarush/test/level34/lesson06/task01/Solution.java
@@ -1,44 +1,37 @@
-package com.javarush.test.level34.lesson04.task01;
+package com.javarush.test.level34.lesson06.task01;
 
-import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
 
-/* Мягкие ссылки
+/* Слабые ссылки
 Разберитесь в примере.
-Внутри метода main в нужном месте создайте мягкую ссылку reference на объект monkey.
-Должны быть только используемые импорты.
+Внутри метода main в нужном месте создайте слабую ссылку reference на объект monkey.
 */
-public class Solution
-{
+public class Solution {
     public static Helper helper = new Helper();
 
-    public static class Monkey
-    {
+    public static class Monkey {
         private String name;
 
-        public Monkey(String name)
-        {
+        public Monkey(String name) {
             this.name = name;
         }
 
-        protected void finalize()
-        {
+        protected void finalize() {
             Helper.isFinalized = true;
             System.out.format("Bye-Bye, %s!\n", name);
         }
     }
 
-    public static void main(String args[]) throws InterruptedException
-    {
+   public static void main(String args[]) throws InterruptedException {
         helper.startTime();
 
         Monkey monkey = new Monkey("Simka");
 
         helper.callGC();
 
-        SoftReference<Monkey> reference = new SoftReference<Monkey>(monkey);
         monkey = null;
+
         helper.callGC();
         helper.heapConsuming();
 
@@ -48,46 +41,36 @@ public class Solution
         helper.finish();
     }
 
-    public static class Helper
-    {
+    public static class Helper {
         public static boolean isFinalized;
 
         private long startTime;
 
-        void startTime()
-        {
+        void startTime() {
             this.startTime = System.currentTimeMillis();
         }
 
-        int getTime()
-        {
+        int getTime() {
             return (int) (System.currentTimeMillis() - startTime) / 1000;
         }
 
-        void callGC() throws InterruptedException
-        {
+        void callGC() throws InterruptedException {
             System.gc();
             Thread.sleep(1000);
         }
 
-        void heapConsuming()
-        {
-            try
-            {
+        void heapConsuming() {
+            try {
                 List<Solution> heap = new ArrayList<Solution>(100000);
-                while (!isFinalized)
-                {
+                while (!isFinalized) {
                     heap.add(new Solution());
                 }
-            }
-            catch (OutOfMemoryError e)
-            {
+            } catch (OutOfMemoryError e) {
                 System.out.println("Out of memory error raised");
             }
         }
 
-        public void finish()
-        {
+        public void finish() {
             System.out.println("Done");
             System.out.println("It took " + getTime() + " sec");
         }
