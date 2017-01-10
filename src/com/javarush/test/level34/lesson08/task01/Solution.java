11b2fce4bb41d8fee51aabe9e8c1ab1cc5bd7015
Andrey Butko <andrey.butko@gmail.com>
2017-01-10 23:33:08 +0200

    Update

--- src/com/javarush/test/level34/lesson08/task01/Solution.java
+++ src/com/javarush/test/level34/lesson08/task01/Solution.java
@@ -12,13 +12,16 @@ import java.util.List;
 2) добавьте в список 200 ссылок, используйте очередь helper.getQueue()
 3) верните заполненный список
 */
-public class Solution {
+public class Solution
+{
     public static Helper helper = new Helper();
 
-    public static class Monkey {
+    public static class Monkey
+    {
     }
 
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String args[]) throws InterruptedException
+    {
         helper.startTime();
         List<PhantomReference<Monkey>> list = helper.getFilledList();
 
@@ -34,43 +37,56 @@ public class Solution {
         helper.finish();
     }
 
-    public static class Helper {
+    public static class Helper
+    {
         private ReferenceQueue<Monkey> queue = new ReferenceQueue<>();
 
         private long startTime;
 
-        void startTime() {
+        void startTime()
+        {
             this.startTime = System.currentTimeMillis();
         }
 
-        int getTime() {
+        int getTime()
+        {
             return (int) (System.currentTimeMillis() - startTime) / 1000;
         }
 
-        void callGC() throws InterruptedException {
+        void callGC() throws InterruptedException
+        {
             System.gc();
             Thread.sleep(1000);
         }
 
-        public ReferenceQueue<Monkey> getQueue() {
+        public ReferenceQueue<Monkey> getQueue()
+        {
             return queue;
         }
 
-        void heapConsuming() {
-            try {
+        void heapConsuming()
+        {
+            try
+            {
                 List<Solution> heap = new ArrayList<Solution>(100000);
-                while (true) {
+                while (true)
+                {
                     heap.add(new Solution());
                 }
-            } catch (OutOfMemoryError e) {
+            }
+            catch (OutOfMemoryError e)
+            {
                 System.out.println("Out of memory error raised");
             }
         }
 
-        public void checkListWithReferences(List<PhantomReference<Monkey>> list, String string) {
+        public void checkListWithReferences(List<PhantomReference<Monkey>> list, String string)
+        {
             int count = 0;
-            for (PhantomReference<Monkey> reference : list) {
-                if (reference.isEnqueued()) {
+            for (PhantomReference<Monkey> reference : list)
+            {
+                if (reference.isEnqueued())
+                {
                     count++;
                 }
             }
@@ -78,13 +94,21 @@ public class Solution {
             System.out.println(String.format("The enqueue reference count is %d (%s GC was called)", count, string));
         }
 
-        public List<PhantomReference<Monkey>> getFilledList() {
-            return null;
+        public List<PhantomReference<Monkey>> getFilledList()
+        {
+            List<PhantomReference<Monkey>> list = new ArrayList<>();
+            for (int i = 0; i < 200; i++)
+            {
+                list.add(new PhantomReference<Monkey>(new Monkey(),getQueue()));
+            }
+            return list;
         }
 
-        public void finish() throws InterruptedException {
+        public void finish() throws InterruptedException
+        {
             int count = 0;
-            while (queue.poll() != null) {
+            while (queue.poll() != null)
+            {
                 count++;
             }
             System.out.println(count + " objects are in the queue of phantom reference");
