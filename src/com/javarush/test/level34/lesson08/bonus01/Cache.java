ef24f3da3cdc6f61f1066407ef9ce92c160d9545
Andrey Butko <andrey.butko@gmail.com>
2017-01-13 17:14:46 +0200

    Update

--- src/com/javarush/test/level34/lesson08/bonus01/Cache.java
+++ src/com/javarush/test/level34/lesson08/bonus01/Cache.java
@@ -1,21 +1,47 @@
 package com.javarush.test.level34.lesson08.bonus01;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.WeakHashMap;
 
-public class Cache<K, V> {
-    private Map<K, V> cache = null;   //TODO add your code here
+public class Cache<K, V>
+{
+    private Map<K, V> cache = new WeakHashMap<K, V>();   //TODO add your code here
 
-    public V getByKey(K key, Class<V> clazz) throws Exception {
-        //TODO add your code here
-        return null;
+    public V getByKey(K key, Class<V> clazz) throws Exception
+    {
+        V obj;
+        if (cache.containsKey(key))
+            obj = cache.get(key);
+        else
+        {
+            Constructor<V> constructor = clazz.getConstructor(key.getClass());
+            obj = constructor.newInstance(key);
+            put(obj);
+        }
+        return obj;
     }
 
-    public boolean put(V obj) {
+    public boolean put(V obj)
+    {
         //TODO add your code here
-        return false;
+        try
+        {
+            Method method = obj.getClass().getDeclaredMethod("getKey");
+            method.setAccessible(true);
+            K key = (K) method.invoke(obj);
+            cache.put(key, obj);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 
-    public int size() {
+    public int size()
+    {
         return cache.size();
     }
 }
